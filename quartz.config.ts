f5cce6863cbe59fc6c889d5a21634817af934f6f
Outerblog <sunjack80@gmail.com>
2024-12-08 15:30:46 +1300

    Quartz sync: Dec 8, 2024, 3:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "refactored-lamp 💡",
+    pageTitle: "refactored lamp 💡",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
